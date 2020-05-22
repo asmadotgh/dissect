255d27be0e54ecb5ef6420d7437ea9d738665ab2
asmadotgh <asma_gh@mit.edu>
2020-05-21 22:47:46 -0400

    making GAN network for the synthetic case smaller, updating regulazier to a working version

--- explainer/networks_64.py
+++ explainer/networks_64.py
@@ -19,9 +19,9 @@ class Generator_Encoder_Decoder:
             inputs = relu(conditional_batchnorm(inputs, train_phase, "BN1"))
             inputs = conv("conv1", inputs, k_size=3, nums_out=64, strides=1)  # [n, 64, 64, 64]
             print(':', inputs)
-            inputs = G_Resblock_Encoder("Encoder-ResBlock4", inputs, 256, train_phase, y,
-                                        nums_class)  # [n, 32, 32, 256]
-            print(':', inputs)
+            # inputs = G_Resblock_Encoder("Encoder-ResBlock4", inputs, 256, train_phase, y,
+            #                             nums_class)  # [n, 32, 32, 256]
+            # print(':', inputs)
             inputs = G_Resblock_Encoder("Encoder-ResBlock3", inputs, 512, train_phase, y,
                                         nums_class)  # [n, 16, 16, 512]
             print(':', inputs)
@@ -42,8 +42,8 @@ class Generator_Encoder_Decoder:
             print(':', inputs)
             inputs = G_Resblock("ResBlock3", inputs, 256, train_phase, y, nums_class)  # [n, 32, 32, 256]
             print(':', inputs)
-            inputs = G_Resblock("ResBlock4", inputs, 128, train_phase, y, nums_class)  # [n, 64, 64, 128]
-            print(':', inputs)
+            # inputs = G_Resblock("ResBlock4", inputs, 128, train_phase, y, nums_class)  # [n, 64, 64, 128]
+            # print(':', inputs)
             inputs = relu(conditional_batchnorm(inputs, train_phase, "BN"))
             inputs = conv("conv", inputs, k_size=3, nums_out=num_channel, strides=1)  # [n, 64, 64, 3]
             print(':', inputs)
@@ -59,7 +59,7 @@ class Discriminator_Ordinal:
 
     def __call__(self, inputs, y, nums_class, update_collection=None):
         with tf.variable_scope(name_or_scope=self.name, reuse=tf.AUTO_REUSE):
-            # input: [n, 6, 64, 3]
+            # input: [n, 64, 64, 3]
             print(inputs)
             inputs = D_FirstResblock("ResBlock1", inputs, 64, update_collection, is_down=True)  # [n, 32, 32, 64]
             print(inputs)
@@ -69,8 +69,8 @@ class Discriminator_Ordinal:
             print(inputs)
             inputs = D_Resblock("ResBlock4", inputs, 512, update_collection, is_down=True)  # [n, 4, 4, 512]
             print(inputs)
-            inputs = D_Resblock("ResBlock5", inputs, 1024, update_collection, is_down=True)  # [n, 2, 2, 1024]
-            print(inputs)
+            # inputs = D_Resblock("ResBlock5", inputs, 1024, update_collection, is_down=True)  # [n, 2, 2, 1024]
+            # print(inputs)
             inputs = relu(inputs)
             print(inputs)  # [n, 2, 2, 1024]
             inputs = global_sum_pooling(inputs)  # [n, 1024]
@@ -88,25 +88,48 @@ class Discriminator_Ordinal:
 
 
 class Discriminator_Contrastive:
-    # Compares embedding of source and perturbed image and determines which "knob" has been shifted
+    # Compares two images and determines which "knob" has been shifted
     def __init__(self, name='disentangler'):
         self.name = name
 
-    def __call__(self, inputs, train_phase, nums_dim):
+    def __call__(self, inputs, num_dims):
         with tf.variable_scope(name_or_scope=self.name, reuse=tf.AUTO_REUSE):
-            # input: [n, 4, 4, 2048]
+            # input: [n, 64, 64, 6]
             print(inputs)
-            inputs = tf.layers.flatten(inputs)
+            inputs = D_FirstResblock("ResBlock1", inputs, 64, None, is_down=True)  # [n, 32, 32, 64]
             print(inputs)
-            inputs = dense_layer(inputs, 256, train_phase, 'RBlock1')  # [n, 256]
+            inputs = D_Resblock("ResBlock2", inputs, 128, None, is_down=True)  # [n, 16, 16, 128]
             print(inputs)
-            inputs = dense_layer(inputs, 128, train_phase, 'RBlock2')  # [n, 128]
+            inputs = relu(inputs)
+            print(inputs)  # [n, 16, 16, 128]
+            inputs = global_sum_pooling(inputs)  # [n, 128]
             print(inputs)
-            inputs = dense_layer(inputs, 64, train_phase, 'RBlock3')  # [n, 64]
-            print(inputs)
-            inputs = dense_layer(inputs, nums_dim, train_phase, 'RBlock4')  # [n, nums_dim]
-            print(inputs)
+            inputs = dense("dense", inputs, num_dims, None, is_sn=True)  # [n, num_dims]
             return inputs
 
     def var_list(self):
         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.name)
+
+# class Discriminator_Contrastive:
+#     # Compares two images and determines which "knob" has been shifted
+#     def __init__(self, name='disentangler'):
+#         self.name = name
+#
+#     def __call__(self, inputs, num_dims):
+#         with tf.variable_scope(name_or_scope=self.name, reuse=tf.AUTO_REUSE):
+#             # input: [n, 64, 64, 6]
+#             print(inputs)
+#             inputs = conv_pooling_layer(inputs, 64, 'Block-1', k_size=4, strides=2)
+#             print(inputs)
+#             inputs = conv_pooling_layer(inputs, 128, 'Block-2', k_size=4, strides=2)
+#             print(inputs)
+#             inputs = tf.layers.flatten(inputs, 'Block-3-flatten')
+#             print(inputs)
+#             inputs = dense_relu_layer('Block-4', inputs, 512)
+#             print(inputs)
+#             inputs = dense_relu_layer('Block-5', inputs, num_dims)
+#             print(inputs)
+#             return inputs
+#
+#     def var_list(self):
+#         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.name)
